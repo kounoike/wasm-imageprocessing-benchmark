acbf45704cf7c54f459b4338611480ea93d3e371
KOUNOIKE Yuusuke <kounoike.yuusuke@gmail.com>
2021-11-28 16:46:46 +0900

    format

--- benchmark/wasm/rust/src/lib.rs
+++ benchmark/wasm/rust/src/lib.rs
@@ -4,19 +4,19 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub struct Screen {
-    input_bytes: Vec<u8>,
-    output_bytes: Vec<f32>,
-    #[wasm_bindgen(readonly)]
-    pub width: usize,
-    #[wasm_bindgen(readonly)]
-    pub height: usize,
+  input_bytes: Vec<u8>,
+  output_bytes: Vec<f32>,
+  #[wasm_bindgen(readonly)]
+  pub width: usize,
+  #[wasm_bindgen(readonly)]
+  pub height: usize,
 }
 
 fn create_buffer_u8(width: usize, height: usize, channel: usize) -> Vec<u8> {
-    let size = width * height * channel;
-    let mut bytes = Vec::with_capacity(size);
-    bytes.resize(size, 0);
-    bytes
+  let size = width * height * channel;
+  let mut bytes = Vec::with_capacity(size);
+  bytes.resize(size, 0);
+  bytes
 }
 
 fn create_buffer_f32(width: usize, height: usize, channel: usize) -> Vec<f32> {
@@ -28,40 +28,40 @@ fn create_buffer_f32(width: usize, height: usize, channel: usize) -> Vec<f32> {
 
 #[wasm_bindgen]
 impl Screen {
-    #[wasm_bindgen(constructor)]
-    pub fn new(width: usize, height: usize) -> Screen {
-        Screen {
-            input_bytes: create_buffer_u8(width, height, 4),
-            output_bytes: create_buffer_f32(width, height, 3),
-            width,
-            height,
-        }
+  #[wasm_bindgen(constructor)]
+  pub fn new(width: usize, height: usize) -> Screen {
+    Screen {
+      input_bytes: create_buffer_u8(width, height, 4),
+      output_bytes: create_buffer_f32(width, height, 3),
+      width,
+      height,
     }
+  }
 
-    pub fn input_pointer(&self) -> *const u8 {
-        self.input_bytes.as_ptr()
-    }
+  pub fn input_pointer(&self) -> *const u8 {
+    self.input_bytes.as_ptr()
+  }
 
-    pub fn input_size(&self) -> usize {
-        self.input_bytes.len()
-    }
+  pub fn input_size(&self) -> usize {
+    self.input_bytes.len()
+  }
 
-    pub fn output_pointer(&self) -> *const f32 {
-        self.output_bytes.as_ptr()
-    }
+  pub fn output_pointer(&self) -> *const f32 {
+    self.output_bytes.as_ptr()
+  }
 
-    pub fn output_size(&self) -> usize {
-        self.output_bytes.len()
-    }
+  pub fn output_size(&self) -> usize {
+    self.output_bytes.len()
+  }
 }
 
 #[wasm_bindgen]
 pub fn exec(screen: &mut Screen) {
-    for i in 0..screen.width * screen.height {
-        screen.output_bytes[i * 3] = (screen.input_bytes[i * 4] as f32) / 255f32;
-        screen.output_bytes[i * 3 + 1] = (screen.input_bytes[i * 4 + 1] as f32) / 255f32;
-        screen.output_bytes[i * 3 + 2] = (screen.input_bytes[i * 4 + 2] as f32) / 255f32;
-    }
+  for i in 0..screen.width * screen.height {
+    screen.output_bytes[i * 3] = (screen.input_bytes[i * 4] as f32) / 255f32;
+    screen.output_bytes[i * 3 + 1] = (screen.input_bytes[i * 4 + 1] as f32) / 255f32;
+    screen.output_bytes[i * 3 + 2] = (screen.input_bytes[i * 4 + 2] as f32) / 255f32;
+  }
 }
 
 // #[wasm_bindgen]
@@ -72,4 +72,3 @@ pub fn exec(screen: &mut Screen) {
 //         screen.output_bytes[i * 3 + 1] = screen.input_bytes[i * 4].saturating_add(10);
 //     }
 // }
-
